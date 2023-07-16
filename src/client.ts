63f033293c9501802382a46fb0531c5bf2508460
freitzzz <1160907@isep.ipp.pt>
2023-07-16 12:03:30 +0100

    style: format

--- src/client.ts
+++ src/client.ts
@@ -2,44 +2,42 @@
  * Alias for positional parameters that most API calls can take in.
  */
 export type LoggingExtraParameters = {
-    extras?: Map<string, string>,
+	extras?: Map<string, string>;
 };
 
 /**
  * An interface for logging messages. Extend it to your own desires and styling and then put it to work with {@link putLumberdashToWork} function.
  */
 export interface LumberdashClient {
+	/**
+	 * Logs an informational message.
+	 *
+	 * @param message - content of the message as string.
+	 * @param extras - a set of additional values that may be useful for the client when logging the message.
+	 */
+	logMessage(message: string, extras: LoggingExtraParameters): void;
 
-    /**
-     * Logs an informational message.
-     * 
-     * @param message - content of the message as string. 
-     * @param extras - a set of additional values that may be useful for the client when logging the message.
-     */
-    logMessage(message: string, extras: LoggingExtraParameters): void;
+	/**
+	 * Logs a warning message.
+	 *
+	 * @param message - content of the message as string.
+	 * @param extras - a set of additional values that may be useful for the client when logging the message.
+	 */
+	logWarning(message: string, extras: LoggingExtraParameters): void;
 
-    /**
-     * Logs a warning message.
-     * 
-     * @param message - content of the message as string. 
-     * @param extras - a set of additional values that may be useful for the client when logging the message.
-     */
-    logWarning(message: string, extras: LoggingExtraParameters): void;
+	/**
+	 * Logs a fatal message. This call is different than {@link logError} in the sense that it should be read as the last message before the program shutdown's
+	 * due to a fatal error.
+	 *
+	 * @param message - content of the message as string.
+	 * @param extras - a set of additional values that may be useful for the client when logging the message.
+	 */
+	logFatal(message: string, extras: LoggingExtraParameters): void;
 
-
-    /**
-     * Logs a fatal message. This call is different than {@link logError} in the sense that it should be read as the last message before the program shutdown's 
-     * due to a fatal error.
-     * 
-     * @param message - content of the message as string. 
-     * @param extras - a set of additional values that may be useful for the client when logging the message.
-     */
-    logFatal(message: string, extras: LoggingExtraParameters): void;
-
-    /**
-     * Logs an error message.
-     * 
-     * @param error - content of the error.
-     */
-    logError(error: Error): void;
+	/**
+	 * Logs an error message.
+	 *
+	 * @param error - content of the error.
+	 */
+	logError(error: Error): void;
 }
--- src/lumberdash.ts
+++ src/lumberdash.ts
@@ -1,4 +1,4 @@
-import { LoggingExtraParameters, LumberdashClient } from "./client";
+import { LoggingExtraParameters, LumberdashClient } from './client';
 
 /**
  * Global list of {@link LumberdashClient} used to log messages.
@@ -11,52 +11,61 @@ const clients = new Array<LumberdashClient>();
 const DefaultExtrasParameters: LoggingExtraParameters = { extras: undefined };
 
 /**
- * Initializes Lumberdash with multiple clients that will log to different buffers 
+ * Initializes Lumberdash with multiple clients that will log to different buffers
  * when any top level log function is called.
- * 
+ *
  * @param withClients - the list of {@link LumberdashClient} that will be used to log messages.
  */
 export function putLumberdashToWork(withClients: LumberdashClient[]) {
-    clients.length = 0;
-    clients.push(...withClients);
+	clients.length = 0;
+	clients.push(...withClients);
 }
 
 /**
  * Logs an informational message.
- * 
- * @param message - content of the message as string. 
+ *
+ * @param message - content of the message as string.
  * @param extras - a set of additional values that may be useful for the client when logging the message.
  */
-export function logMessage(message: string, { extras } = DefaultExtrasParameters): void {
-    clients.forEach((c) => c.logMessage(message, { extras: extras }));
+export function logMessage(
+	message: string,
+	{ extras } = DefaultExtrasParameters
+): void {
+	clients.forEach((c) => c.logMessage(message, { extras: extras }));
 }
 
 /**
  * Logs a warning message.
- * 
- * @param message - content of the message as string. 
+ *
+ * @param message - content of the message as string.
  * @param extras - a set of additional values that may be useful for the client when logging the message.
  */
-export function logWarning(message: string, { extras } = DefaultExtrasParameters): void {
-    clients.forEach((c) => c.logWarning(message, { extras: extras }));
+export function logWarning(
+	message: string,
+	{ extras } = DefaultExtrasParameters
+): void {
+	clients.forEach((c) => c.logWarning(message, { extras: extras }));
 }
 
 /**
- * Logs a fatal message. This call is different than {@link logError} in the sense that it should be read as the last message before the program shutdown's 
+ * Logs a fatal message. This call is different than {@link logError} in the sense that it should be read as the last message before the program shutdown's
  * due to a fatal error.
- * 
- * @param message - content of the message as string. 
+ *
+ * @param message - content of the message as string.
  * @param extras - a set of additional values that may be useful for the client when logging the message.
  */
-export function logFatal(message: string, { extras } = DefaultExtrasParameters): void {
-    clients.forEach((c) => c.logFatal(message, { extras: extras }));
+export function logFatal(
+	message: string,
+	{ extras } = DefaultExtrasParameters
+): void {
+	clients.forEach((c) => c.logFatal(message, { extras: extras }));
 }
 
 /**
  * Logs an error message.
- * 
+ *
  * @param error - content of the error.
  */
 export function logError(error: Error): void {
-    clients.forEach((c) => c.logError(error));
+	clients.forEach((c) => c.logError(error));
 }
