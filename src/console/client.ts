12cca468aafe240981e61763889a8cc5e6342791
freitzzz <1160907@isep.ipp.pt>
2023-07-16 12:10:14 +0100

    feat: implement console lumberdash client

---
+++ src/console/client.ts
@@ -0,0 +1,22 @@
+import { LoggingExtraParameters, LumberdashClient } from "../lumberdash";
+
+/**
+ * An implementation of {@link LumberdashClient} that uses {@link console} API to log messages in the terminal/browser console.
+ */
+export class ConsoleLumberdashClient implements LumberdashClient {
+    logMessage(message: string, extras: LoggingExtraParameters): void {
+        console.info(message, extras);
+    }
+
+    logWarning(message: string, extras: LoggingExtraParameters): void {
+        console.warn(message, extras);
+    }
+
+    logFatal(message: string, extras: LoggingExtraParameters): void {
+        console.trace(message, extras);
+    }
+
+    logError(error: Error): void {
+        console.error(error);
+    }
+}
